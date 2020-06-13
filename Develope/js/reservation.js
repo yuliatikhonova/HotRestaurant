eeefdf6bcb02cf43b0b74475324f182259fd3583
yulia tikhonova <yuliatikhonova@outlook.com>
2020-06-13 12:03:41 -0700

    was able to add json data

---
+++ Develope/js/reservation.js
@@ -0,0 +1,10 @@
+const reservation = [
+    {
+        id: "3",
+        name: "Me",
+        email: "me@yahoo.com",
+        phone: "480-423-5234"
+    }
+]
+
+module.exports = reservation;
--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const waiting = require("./Develope/js/waitlist")
+const reservation = require("./Develope/js/reservation")
 const app = express();
 
 const PORT = 3000;
+
+// app.get("/", function(req, res) {//when it gets a request to / path 
+//     res.send("Welcome to the Star Wars Page!");//text
+//   });
+
+  app.get("/waiting", function(req, res) {
+    res.json(waiting);//json data
+  });
+  
+
+app.get("/reservation", function (req, res) {
+    res.json(reservation);//json data
+});
 
 app.listen(PORT, function () {
     console.log('Listening at PORT 3000');
